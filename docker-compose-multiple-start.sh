932b474980338b05c6e198ce74fbf98c75861c3a
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2022-03-07 16:31:39 +0100

    use dynamic naming in scan

--- docker-compose-multiple-start.sh
+++ docker-compose-multiple-start.sh
@@ -4,6 +4,6 @@ echo "Starting ..."
 docker-compose -f docker-compose-multiple1.yml up -d
 
 echo "Scanning for new files..."
-docker exec -u www-data nc php occ files:scan admin
+docker exec -u www-data nc_${USERNAME} php occ files:scan admin
 
 echo "Ready"
