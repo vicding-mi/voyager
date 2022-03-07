06567348b5b8015e464688642871c67ebac6fd1e
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2022-03-07 16:24:07 +0100

    fix mutiple container name conflict

--- docker-compose-multiple-create.sh
+++ docker-compose-multiple-create.sh
@@ -24,11 +24,11 @@ done
 echo "NC populated"
 
 echo "Installing NC..."
-docker exec -u www-data nc php occ maintenance:install --admin-user admin --admin-pass ba222ded25d957b900c03bef914333cd
+docker exec -u www-data nc_${USERNAME} php occ maintenance:install --admin-user admin --admin-pass ba222ded25d957b900c03bef914333cd
 
 echo "Installing text editor..."
-docker exec nc curl -L -o /tmp/files_texteditor.tar.gz https://github.com/nextcloud-releases/files_texteditor/releases/download/v2.14.0/files_texteditor.tar.gz
-docker exec nc tar -zxf /tmp/files_texteditor.tar.gz -C /var/www/html/apps/
+docker exec nc_${USERNAME} curl -L -o /tmp/files_texteditor.tar.gz https://github.com/nextcloud-releases/files_texteditor/releases/download/v2.14.0/files_texteditor.tar.gz
+docker exec nc_${USERNAME} tar -zxf /tmp/files_texteditor.tar.gz -C /var/www/html/apps/
 rm -f /tmp/files_texteditor.tar.gz
 
 echo "Stopping building script"
@@ -39,20 +39,23 @@ echo "Starting running script"
 docker-compose -f docker-compose-multiple1.yml up -d
 
 echo "Make sure file permission is well set"
-docker exec -u root nc chown -R www-data:root /var/www/html/
+docker exec -u root nc_${USERNAME} chown -R www-data:root /var/www/html/
 
 echo "Disable default plain text editor, firstrunwizard and dashboard"
-docker exec -u www-data nc php occ app:disable text
-docker exec -u www-data nc php occ app:disable firstrunwizard
-docker exec -u www-data nc php occ app:disable dashboard
+docker exec -u www-data nc_${USERNAME} php occ app:disable text
+docker exec -u www-data nc_${USERNAME} php occ app:disable firstrunwizard
+docker exec -u www-data nc_${USERNAME} php occ app:disable dashboard
 
 echo "Enable text editor"
-docker exec -u www-data nc php occ app:enable files_texteditor
+docker exec -u www-data nc_${USERNAME} php occ app:enable files_texteditor
 
 echo "Scanning for newly added files..."
-docker exec -u www-data nc php occ files:scan admin
+docker exec -u www-data nc_${USERNAME} php occ files:scan admin
 
 echo "Adding trusted domain"
-docker exec -u www-data nc php occ config:system:set trusted_domains 2 --value=${USERNAME}.${HOSTNAME}
+docker exec -u www-data nc_${USERNAME} php occ config:system:set trusted_domains 2 --value=${USERNAME}.${HOSTNAME}
 
-echo "Ready"
+echo "Stopping stack after init run"
+./docker-compose-multiple-stop.sh
+
+echo "Ready! Run start script to start the stack"
