fe24219830180663c299912e6bacc2fe75f5d92e
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2022-03-07 17:58:16 +0100

    add url hint

--- docker-compose-multiple-start.sh
+++ docker-compose-multiple-start.sh
@@ -8,4 +8,8 @@ docker-compose -f docker-compose-multiple1.yml up -d
 echo "Scanning for new files..."
 docker exec -u www-data nc_${USERNAME} php occ files:scan admin
 
+echo "### Please use the following link to access nextcloud and voyager ###"
+echo "For nextcloud: https://nc_${USERNAME}.${HOSTNAME} with username admin"
+echo "For voyager: https://voyager_${USERNAME}.${HOSTNAME}/voyager-story-dev.html?root=models/<your folder name>/&document=<your json file>.json"
+
 echo "Ready"
