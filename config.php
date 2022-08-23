2598afc5d4629d05c9b5eab98578af05971af4ef
Vic Ding <qiqing.ding@di.huc.knaw.nl>
2022-08-23 11:02:55 +0200

    add aton

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+    /* OIDC TEST */
+   'oidc_login_provider_url' => 'https://testauthentication.di.huc.knaw.nl',
+   'oidc_login_client_id' => 'nl_clariah_dev_pure3d_files',
+   'oidc_login_client_secret' => '86101d42-e7f7-11ec-852a-0b76b2c55d91',
+   'oidc_login_scope' => 'openid email profile',
+    'oidc_login_disable_registration' => false,
+   'oidc_login_attributes' => array (
+        'id' => 'sub',
+        'name' => 'nickname',
+        'mail' => 'email',
+    ),
